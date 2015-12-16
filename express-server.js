ff86017d0281a55d07af984c830988caf2fc3583
Alexander Juarez <tall@fb.com>
2015-12-15 17:47:45 -0800

    updated server to take a port argument

--- express-server.js
+++ express-server.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 3000;
+
 app.use('/', express.static(__dirname + '/dist'));
 app.use('/fonts', express.static(__dirname + '/src/fonts'));
 
-app.listen(3000);
+app.listen(port, function() {
+  console.log('the server is running at http://localhost:' + port);
+});
