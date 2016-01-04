76ac26537fd956da07162acf86be86fdc743d8c4
Alexander Juarez <tall@fb.com>
2016-01-04 14:32:04 -0800

    added new hero badge

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,6 +12,7 @@ const mentorBadgeSVG = require('../images/badges/mentor.svg');
 const interviewerBadgeSVG = require('../images/badges/interviewer.svg');
 const hackerBadgeSVG = require('../images/badges/hack-presentor.svg');
 const i18nBadgeSVG = require('../images/badges/i18n.svg');
+const fbHeroBadgeSVG = require('../images/badges/facebook-hero.svg');
 
 import React from 'react';
 import { Link } from 'react-router';
@@ -23,6 +24,7 @@ class AppComponent extends React.Component {
         <h1>Badges svg 64px</h1>
         <ul className="list">
           <li className="shine star"><img height="64" width="64" src={interviewBadgeSVG} /></li>
+          <li className="shine star"><img height="64" width="64" src={fbHeroBadgeSVG} /></li>
           <li className="shine circle"><img height="64" width="64" src={bugPatrolBadgeSVG} /></li>
           <li className="shine circle"><img height="64" width="64" src={wikiContribBadgeSVG} /></li>
           <li className="shine circle"><img height="64" width="64" src={fiveOfficesBadgeSVG} /></li>
@@ -36,6 +38,7 @@ class AppComponent extends React.Component {
         <h1>Badges svg 32px</h1>
         <ul className="list">
           <li className="shine sm star"><img height="32" width="32" src={interviewBadgeSVG} /></li>
+          <li className="shine sm star"><img height="32" width="32" src={fbHeroBadgeSVG} /></li>
           <li className="shine sm circle"><img height="32" width="32" src={bugPatrolBadgeSVG} /></li>
           <li className="shine sm circle"><img height="32" width="32" src={wikiContribBadgeSVG} /></li>
           <li className="shine sm circle"><img height="32" width="32" src={fiveOfficesBadgeSVG} /></li>
